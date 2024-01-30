aae46eb24a28a3e69f098b93e15fe2c3368c7eb0
Tirlim6om6om <57911899+Tirlim6om6om@users.noreply.github.com>
2024-01-30 22:41:07 +0300

    Локальный пользователь

--- Assets/Code/Scripts/HardwareRig.cs
+++ Assets/Code/Scripts/HardwareRig.cs
@@ -23,97 +23,62 @@ public class HardwareRig : MonoBehaviour, INetworkRunnerCallbacks
     void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input)
     {
         RigState xrRigState = new RigState();
-        xrRigState.PlayerPosition = playerTransform.position;
-        xrRigState.PlayerRotation = playerTransform.rotation;
         UxrAvatarRig rig = avatar.AvatarRig;
         
-        //Основа
-        xrRigState.UpperChest.Set(rig.UpperChest.position, rig.UpperChest.rotation);
-        xrRigState.Chest.Set(rig.Chest.position, rig.UpperChest.rotation);
-        xrRigState.Spine.Set(rig.Spine.position, rig.Spine.rotation);
-        xrRigState.Hips.Set(rig.Hips.position, rig.Hips.rotation);
-
-
-        #region arm left
-        //ARM LEFT
-        xrRigState.LeftArm.Clavicle.Set(rig.LeftArm.Clavicle.position, rig.LeftArm.Clavicle.rotation);
-        xrRigState.LeftArm.UpperArm.Set(rig.LeftArm.UpperArm.position, rig.LeftArm.UpperArm.rotation);
-        xrRigState.LeftArm.Forearm.Set(rig.LeftArm.Forearm.position, rig.LeftArm.Forearm.rotation);
-        //HandLeft
-        xrRigState.LeftArm.Hand.Wrist.Set(rig.LeftArm.Hand.Wrist.position, rig.LeftArm.Hand.Wrist.rotation);
-        //FINGERS LEFT
-        //Thumb
-        xrRigState.LeftArm.Hand.Thumb.Intermediate.Set(rig.LeftArm.Hand.Thumb.Intermediate.position, rig.LeftArm.Hand.Thumb.Intermediate.rotation);
-        xrRigState.LeftArm.Hand.Thumb.Metacarpal.Set(rig.LeftArm.Hand.Thumb.Metacarpal.position, rig.LeftArm.Hand.Thumb.Metacarpal.rotation);
-        xrRigState.LeftArm.Hand.Thumb.Proximal.Set(rig.LeftArm.Hand.Thumb.Proximal.position, rig.LeftArm.Hand.Thumb.Proximal.rotation);
-        xrRigState.LeftArm.Hand.Thumb.Distal.Set(rig.LeftArm.Hand.Thumb.Distal.position, rig.LeftArm.Hand.Thumb.Distal.rotation);
-        //Index
-        xrRigState.LeftArm.Hand.Index.Intermediate.Set(rig.LeftArm.Hand.Index.Intermediate.position, rig.LeftArm.Hand.Index.Intermediate.rotation);
-        xrRigState.LeftArm.Hand.Index.Metacarpal.Set(rig.LeftArm.Hand.Index.Metacarpal.position, rig.LeftArm.Hand.Index.Metacarpal.rotation);
-        xrRigState.LeftArm.Hand.Index.Proximal.Set(rig.LeftArm.Hand.Index.Proximal.position, rig.LeftArm.Hand.Index.Proximal.rotation);
-        xrRigState.LeftArm.Hand.Index.Distal.Set(rig.LeftArm.Hand.Index.Distal.position, rig.LeftArm.Hand.Index.Distal.rotation);
-        //Middle
-        xrRigState.LeftArm.Hand.Middle.Intermediate.Set(rig.LeftArm.Hand.Middle.Intermediate.position, rig.LeftArm.Hand.Middle.Intermediate.rotation);
-        xrRigState.LeftArm.Hand.Middle.Metacarpal.Set(rig.LeftArm.Hand.Middle.Metacarpal.position, rig.LeftArm.Hand.Middle.Metacarpal.rotation);
-        xrRigState.LeftArm.Hand.Middle.Proximal.Set(rig.LeftArm.Hand.Middle.Proximal.position, rig.LeftArm.Hand.Middle.Proximal.rotation);
-        xrRigState.LeftArm.Hand.Middle.Distal.Set(rig.LeftArm.Hand.Middle.Distal.position, rig.LeftArm.Hand.Middle.Distal.rotation);
-        //Ring
-        xrRigState.LeftArm.Hand.Ring.Intermediate.Set(rig.LeftArm.Hand.Ring.Intermediate.position, rig.LeftArm.Hand.Ring.Intermediate.rotation);
-        xrRigState.LeftArm.Hand.Ring.Metacarpal.Set(rig.LeftArm.Hand.Ring.Metacarpal.position, rig.LeftArm.Hand.Ring.Metacarpal.rotation);
-        xrRigState.LeftArm.Hand.Ring.Proximal.Set(rig.LeftArm.Hand.Ring.Proximal.position, rig.LeftArm.Hand.Ring.Proximal.rotation);
-        xrRigState.LeftArm.Hand.Ring.Distal.Set(rig.LeftArm.Hand.Ring.Distal.position, rig.LeftArm.Hand.Ring.Distal.rotation);
-        //Little
-        xrRigState.LeftArm.Hand.Little.Intermediate.Set(rig.LeftArm.Hand.Little.Intermediate.position, rig.LeftArm.Hand.Little.Intermediate.rotation);
-        xrRigState.LeftArm.Hand.Little.Metacarpal.Set(rig.LeftArm.Hand.Little.Metacarpal.position, rig.LeftArm.Hand.Little.Metacarpal.rotation);
-        xrRigState.LeftArm.Hand.Little.Proximal.Set(rig.LeftArm.Hand.Little.Proximal.position, rig.LeftArm.Hand.Little.Proximal.rotation);
-        xrRigState.LeftArm.Hand.Little.Distal.Set(rig.LeftArm.Hand.Little.Distal.position, rig.LeftArm.Hand.Little.Distal.rotation);
-        
-
-        #endregion
-
-        #region arm right
-        //ARM Right
-        xrRigState.RightArm.Clavicle.Set(rig.RightArm.Clavicle.position, rig.RightArm.Clavicle.rotation);
-        xrRigState.RightArm.UpperArm.Set(rig.RightArm.UpperArm.position, rig.RightArm.UpperArm.rotation);
-        xrRigState.RightArm.Forearm.Set(rig.RightArm.Forearm.position, rig.RightArm.Forearm.rotation);
-        //HandLeft
-        xrRigState.RightArm.Hand.Wrist.Set(rig.RightArm.Hand.Wrist.position, rig.RightArm.Hand.Wrist.rotation);
-        //FINGERS LEFT
-        //Thumb
-        xrRigState.RightArm.Hand.Thumb.Intermediate.Set(rig.RightArm.Hand.Thumb.Intermediate.position, rig.RightArm.Hand.Thumb.Intermediate.rotation);
-        xrRigState.RightArm.Hand.Thumb.Metacarpal.Set(rig.RightArm.Hand.Thumb.Metacarpal.position, rig.RightArm.Hand.Thumb.Metacarpal.rotation);
-        xrRigState.RightArm.Hand.Thumb.Proximal.Set(rig.RightArm.Hand.Thumb.Proximal.position, rig.RightArm.Hand.Thumb.Proximal.rotation);
-        xrRigState.RightArm.Hand.Thumb.Distal.Set(rig.RightArm.Hand.Thumb.Distal.position, rig.RightArm.Hand.Thumb.Distal.rotation);
-        //Index
-        xrRigState.RightArm.Hand.Index.Intermediate.Set(rig.RightArm.Hand.Index.Intermediate.position, rig.RightArm.Hand.Index.Intermediate.rotation);
-        xrRigState.RightArm.Hand.Index.Metacarpal.Set(rig.RightArm.Hand.Index.Metacarpal.position, rig.RightArm.Hand.Index.Metacarpal.rotation);
-        xrRigState.RightArm.Hand.Index.Proximal.Set(rig.RightArm.Hand.Index.Proximal.position, rig.RightArm.Hand.Index.Proximal.rotation);
-        xrRigState.RightArm.Hand.Index.Distal.Set(rig.RightArm.Hand.Index.Distal.position, rig.RightArm.Hand.Index.Distal.rotation);
-        //Middle
-        xrRigState.RightArm.Hand.Middle.Intermediate.Set(rig.RightArm.Hand.Middle.Intermediate.position, rig.RightArm.Hand.Middle.Intermediate.rotation);
-        xrRigState.RightArm.Hand.Middle.Metacarpal.Set(rig.RightArm.Hand.Middle.Metacarpal.position, rig.RightArm.Hand.Middle.Metacarpal.rotation);
-        xrRigState.RightArm.Hand.Middle.Proximal.Set(rig.RightArm.Hand.Middle.Proximal.position, rig.RightArm.Hand.Middle.Proximal.rotation);
-        xrRigState.RightArm.Hand.Middle.Distal.Set(rig.RightArm.Hand.Middle.Distal.position, rig.RightArm.Hand.Middle.Distal.rotation);
-        //Ring
-        xrRigState.RightArm.Hand.Ring.Intermediate.Set(rig.RightArm.Hand.Ring.Intermediate.position, rig.RightArm.Hand.Ring.Intermediate.rotation);
-        xrRigState.RightArm.Hand.Ring.Metacarpal.Set(rig.RightArm.Hand.Ring.Metacarpal.position, rig.RightArm.Hand.Ring.Metacarpal.rotation);
-        xrRigState.RightArm.Hand.Ring.Proximal.Set(rig.RightArm.Hand.Ring.Proximal.position, rig.RightArm.Hand.Ring.Proximal.rotation);
-        xrRigState.RightArm.Hand.Ring.Distal.Set(rig.RightArm.Hand.Ring.Distal.position, rig.RightArm.Hand.Ring.Distal.rotation);
-        //Little
-        xrRigState.RightArm.Hand.Little.Intermediate.Set(rig.RightArm.Hand.Little.Intermediate.position, rig.RightArm.Hand.Little.Intermediate.rotation);
-        xrRigState.RightArm.Hand.Little.Metacarpal.Set(rig.RightArm.Hand.Little.Metacarpal.position, rig.RightArm.Hand.Little.Metacarpal.rotation);
-        xrRigState.RightArm.Hand.Little.Proximal.Set(rig.RightArm.Hand.Little.Proximal.position, rig.RightArm.Hand.Little.Proximal.rotation);
-        xrRigState.RightArm.Hand.Little.Distal.Set(rig.RightArm.Hand.Little.Distal.position, rig.RightArm.Hand.Little.Distal.rotation);
-        #endregion
-        
-        //Голова
-        xrRigState.Head.Set(rig.Head.Head.position,rig.Head.Head.rotation);
+        xrRigState.Player = new PosInfo(playerTransform.position, playerTransform.rotation);
+        xrRigState.UpperChest = new PosInfo(rig.UpperChest.position, rig.UpperChest.rotation);
+        xrRigState.Chest = new PosInfo(rig.Chest.position, rig.UpperChest.rotation);
+        xrRigState.Spine = new PosInfo(rig.Spine.position, rig.Spine.rotation);
+        xrRigState.Hips = new PosInfo(rig.Hips.position, rig.Hips.rotation);
+        xrRigState.LeftArm = CreateArm(rig.LeftArm);
+        xrRigState.RightArm = CreateArm(rig.RightArm);
+        xrRigState.Head = new PosInfo(rig.Head.Head.position, rig.Head.Head.rotation);
         
         input.Set(xrRigState);
         
     }
-
     #endregion
+
+    private Arm CreateArm(UxrAvatarArm avatarArm)
+    {
+        Arm arm = new Arm();
+        arm.Clavicle = new PosInfo(avatarArm.Clavicle.position, avatarArm.Clavicle.rotation);
+        arm.UpperArm = new PosInfo(avatarArm.UpperArm.position, avatarArm.UpperArm.rotation);
+        arm.Forearm = new PosInfo(avatarArm.Forearm.position, avatarArm.Forearm.rotation);
+        arm.Hand = CreateHand(avatarArm.Hand);
+        return arm;
+    }
+
+    private Hand CreateHand(UxrAvatarHand avatarHand)
+    {
+        Hand hand = new Hand();
+        
+        hand.Wrist = new PosInfo(avatarHand.Wrist.position, avatarHand.Wrist.rotation);
+        hand.Thumb = CreateFinger(avatarHand.Thumb);
+        hand.Index = CreateFinger(avatarHand.Index);
+        hand.Middle = CreateFinger(avatarHand.Middle);
+        hand.Ring = CreateFinger(avatarHand.Ring);
+        hand.Little = CreateFinger(avatarHand.Little);
+
+        return hand;
+    }
+
+    private Finger CreateFinger(UxrAvatarFinger avatarFinger)
+    {
+        Finger finger = new Finger();
+        if(avatarFinger.Intermediate != null)
+            finger.Intermediate = new PosInfo(avatarFinger.Intermediate.position, avatarFinger.Intermediate.rotation);
+        
+        if(avatarFinger.Metacarpal != null)
+            finger.Metacarpal = new PosInfo(avatarFinger.Metacarpal.position, avatarFinger.Metacarpal.rotation);
+        
+        if(avatarFinger.Proximal != null)
+            finger.Proximal = new PosInfo(avatarFinger.Proximal.position, avatarFinger.Proximal.rotation);
+        
+        if(avatarFinger.Distal != null)
+            finger.Distal = new PosInfo(avatarFinger.Distal.position, avatarFinger.Distal.rotation);
+        return finger;
+    }
 
     #region Unused INetworkRunnerCallbacks
 
@@ -217,9 +182,7 @@ public class HardwareRig : MonoBehaviour, INetworkRunnerCallbacks
 
 public struct RigState : INetworkInput
 {
-    public Vector3 PlayerPosition;
-    public Quaternion PlayerRotation;
-
+    public PosInfo Player;
     public PosInfo Head;
     public PosInfo UpperChest;
     public PosInfo Chest;
@@ -227,22 +190,11 @@ public struct RigState : INetworkInput
     public PosInfo Hips;
     public Arm LeftArm;
     public Arm RightArm;
-
-    public RigState(int n = 0)
-    {
-        Head = default;
-        PlayerPosition = default;
-        PlayerRotation = default;
-        UpperChest = default;
-        Chest = default;
-        Spine = default;
-        Hips = default;
-        LeftArm = default;
-        RightArm = default;
-    }
+    
+    
 }
 
-public struct Arm
+public struct Arm : INetworkInput
 {
     public PosInfo Clavicle;
     public PosInfo UpperArm;
@@ -250,7 +202,7 @@ public struct Arm
     public Hand Hand;
 }
 
-public struct Hand
+public struct Hand : INetworkInput
 {
     public PosInfo Wrist;
     public Finger Thumb;
@@ -261,7 +213,7 @@ public struct Hand
 }
 
 
-public struct Finger
+public struct Finger : INetworkInput
 {
     public PosInfo Metacarpal;
     public PosInfo Proximal;
@@ -269,14 +221,22 @@ public struct Finger
     public PosInfo Distal;
 }
 
-public struct PosInfo
+public struct PosInfo : INetworkInput
 {
     public Vector3 Pos;
     public Quaternion Rot;
+
+    public PosInfo(Vector3 pos, Quaternion rot)
+    {
+        Pos = pos;
+        Rot = rot;
+    }
 
     public void Set(Vector3 pos, Quaternion rot)
     {
         Pos = pos;
         Rot = rot;
     }
+    
+    
 }
--- Assets/Code/Scripts/NetworkRig.cs
+++ Assets/Code/Scripts/NetworkRig.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using UltimateXR.Avatar;
+using UltimateXR.Avatar.Rig;
+using UltimateXR.Extensions.Unity;
 
 public class NetworkRig : NetworkBehaviour
 {
@@ -11,16 +14,9 @@ public class NetworkRig : NetworkBehaviour
     [SerializeField]
     private NetworkTransform playerTransform;
 
-    [SerializeField]
-    private NetworkTransform headTransform;
+    [SerializeField] private UxrAvatar avatar;
 
-    [SerializeField]
-    private NetworkTransform leftHandTransform;
-
-    [SerializeField]
-    private NetworkTransform rightHandTransform;
-
-    HardwareRig hardwareRig;
+    private HardwareRig hardwareRig;
 
     public override void Spawned()
     {
@@ -41,11 +37,42 @@ public class NetworkRig : NetworkBehaviour
 
         if (GetInput<RigState>(out var input))
         {
-            playerTransform.transform.SetPositionAndRotation(input.PlayerPosition, input.PlayerRotation);
-
-            headTransform.transform.SetPositionAndRotation(input.Head.Pos, input.Head.Rot);
-
+            UxrAvatarRig rig = avatar.AvatarRig;
+            playerTransform.transform.SetPositionAndRotation(input.Player.Pos, input.Player.Rot);
+            rig.Head.Head.SetPositionAndRotation(input.Head.Pos, input.Head.Rot);
+            rig.UpperChest.SetPositionAndRotation(input.UpperChest.Pos, input.UpperChest.Rot);
+            rig.Chest.SetPositionAndRotation(input.Chest.Pos, input.Chest.Rot);
+            rig.Hips.SetPositionAndRotation(input.Hips.Pos, input.Hips.Rot);
+            rig.Spine.SetPositionAndRotation(input.Spine.Pos, input.Spine.Rot);
+            SetArm(input.LeftArm, rig.LeftArm);
+            SetArm(input.RightArm, rig.RightArm);
         }
+    }
+
+    private void SetArm(Arm arm, UxrAvatarArm avatarArm)
+    {
+        avatarArm.Clavicle.SetPositionAndRotation(arm.Clavicle.Pos, arm.Clavicle.Rot);
+        avatarArm.Forearm.SetPositionAndRotation(arm.Clavicle.Pos, arm.Clavicle.Rot);
+        avatarArm.UpperArm.SetPositionAndRotation(arm.UpperArm.Pos, arm.UpperArm.Rot);
+        SetHand(arm.Hand, avatarArm.Hand);
+    }
+
+    private void SetHand(Hand hand, UxrAvatarHand avatarHand)
+    {
+        avatarHand.Wrist.SetPositionAndRotation(hand.Wrist.Pos, hand.Wrist.Rot);
+        SetFingers(hand.Thumb, avatarHand.Thumb);
+        SetFingers(hand.Index, avatarHand.Index);
+        SetFingers(hand.Middle, avatarHand.Middle);
+        SetFingers(hand.Ring, avatarHand.Ring);
+        SetFingers(hand.Little, avatarHand.Little);
+    }
+
+    private void SetFingers(Finger finger, UxrAvatarFinger avatarFinger)
+    {
+        avatarFinger.Distal.SetPositionAndRotation(finger.Distal.Pos, finger.Distal.Rot);
+        avatarFinger.Intermediate.SetPositionAndRotation(finger.Intermediate.Pos, finger.Intermediate.Rot);
+        avatarFinger.Metacarpal.SetPositionAndRotation(finger.Metacarpal.Pos, finger.Metacarpal.Rot);
+        avatarFinger.Proximal.SetPositionAndRotation(finger.Proximal.Pos, finger.Proximal.Rot);
     }
 
     public override void Render()
@@ -53,10 +80,59 @@ public class NetworkRig : NetworkBehaviour
         base.Render();
         if (IsLocalNetworkRig)
         {
-            playerTransform.transform.SetPositionAndRotation(hardwareRig.playerTransform.position, hardwareRig.playerTransform.rotation);
-            
-            headTransform.transform.SetPositionAndRotation(hardwareRig.avatar.AvatarRig.Head.Head.position, hardwareRig.avatar.AvatarRig.Head.Head.rotation);
+            UxrAvatarRig rigNet = avatar.AvatarRig;
+            UxrAvatarRig rigLocal = hardwareRig.avatar.AvatarRig;
+            playerTransform.transform.SetPositionAndRotation(hardwareRig.playerTransform.position,
+                hardwareRig.playerTransform.rotation);
+            //head
+            SetPos(rigNet.Head.Head, rigLocal.Head.Head);
+            SetPos(rigNet.Head.Jaw, rigLocal.Head.Jaw);
+            SetPos(rigNet.Head.Neck, rigLocal.Head.Neck);
+            SetPos(rigNet.Head.LeftEye, rigLocal.Head.LeftEye);
+            SetPos(rigNet.Head.RightEye, rigLocal.Head.RightEye);
 
+            //Chest
+            SetPos(rigNet.UpperChest, rigLocal.UpperChest);
+            SetPos(rigNet.Chest, rigLocal.Chest);
+            SetPos(rigNet.Hips, rigLocal.Hips);
+            SetPos(rigNet.Spine, rigLocal.Spine);
+            
+            //Arms
+            SetArm(rigNet.LeftArm, rigLocal.LeftArm);
+            SetArm(rigNet.RightArm, rigLocal.RightArm);
         }
+    }
+    
+    private void SetArm(UxrAvatarArm arm, UxrAvatarArm avatarArm)
+    {
+        SetPos(arm.Clavicle, avatarArm.Clavicle);
+        SetPos(arm.Forearm, avatarArm.Forearm);
+        SetPos(arm.UpperArm, avatarArm.UpperArm);
+        SetHand(arm.Hand, avatarArm.Hand);
+    }
+
+    private void SetHand(UxrAvatarHand hand, UxrAvatarHand avatarHand)
+    {
+        SetPos(hand.Wrist, avatarHand.Wrist);
+        SetFingers(hand.Thumb, avatarHand.Thumb);
+        SetFingers(hand.Index, avatarHand.Index);
+        SetFingers(hand.Middle, avatarHand.Middle);
+        SetFingers(hand.Ring, avatarHand.Ring);
+        SetFingers(hand.Little, avatarHand.Little);
+    }
+
+    private void SetFingers(UxrAvatarFinger finger, UxrAvatarFinger avatarFinger)
+    {
+        SetPos(finger.Distal, avatarFinger.Distal);
+        SetPos(finger.Intermediate, avatarFinger.Intermediate);
+        SetPos(finger.Metacarpal, avatarFinger.Metacarpal);
+        SetPos(finger.Proximal, avatarFinger.Proximal);
+    }
+    
+    private void SetPos(Transform set, Transform get)
+    {
+        if (get == null || set == null)
+            return;
+        set.SetPositionAndRotation(get.position, get.rotation);
     }
 }
